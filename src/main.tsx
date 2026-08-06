b627e024fff0da86cd4f5afd86ae5e687909cd99
yowaidesu <vinceredondo23@gmail.com>
2026-08-06 18:29:43 +0800

    Fix: Type errors and reinstall router; build successful

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/MY-PORTFOLIO">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 )
