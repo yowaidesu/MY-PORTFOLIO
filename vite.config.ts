09895f1bee457407c8317352f72c34fb7e96bf18
yowaidesu <134474688+yowaidesu@users.noreply.github.com>
2026-08-09 20:00:35 +0800

    Change base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Use root base for Netlify (serve from site root)
-  base: '/',
+  base: '/MY-PORTFOLIO/',
   server: {
     hmr: false,
   },
